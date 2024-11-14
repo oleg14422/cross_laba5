e991a6c4976f155937cee0add8dd34cef190e2c3
oleg14422 <olegkuzmich22144@gmail.com>
2024-11-14 14:40:33 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+const _size = 75.0;
+const _bgColor = Colors.yellow;
+const _borderColor = Colors.grey;
+const _borderWidth = 1.0;
+const _borderRadius = 5.0;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: _buildZero(),
+      ),
+    );
+  }
+
+  List<Widget> _buildZero() {
+    return [
+      _buildLineTop(),
+      _buildLine2nd(),
+      _buildLine3rd(),
+      _buildLine4th(),
+      _buildLine5th(),
+      _buildLineBottom(),
+    ];
+  }
+
+  Widget _buildLineTopBottom() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(Colors.yellow),
+        _buildDot(Colors.yellow),
+      ],
+    );
+  }
+
+  Widget _buildLineSide() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(Colors.yellow),
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildDot(Colors.yellow),
+      ],
+    );
+  }
+
+  Widget _buildLineTop() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(Colors.yellow),
+        _buildDot(Colors.yellow),
+        _buildDot(Colors.yellow),
+        _buildEmpty()
+      ],
+    );
+  }
+
+  Widget _buildLine2nd() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildDot(Colors.yellow),
+      ],
+    );
+  }
+
+  Widget _buildLine3rd() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildDot(Colors.yellow),
+        _buildEmpty(),
+      ],
+    );
+  }
+
+  Widget _buildLine4th() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildEmpty(),
+        _buildDot(Colors.yellow),
+        _buildEmpty(),
+        _buildEmpty(),
+      ],
+    );
+  }
+
+  Widget _buildLine5th() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(Colors.yellow),
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildEmpty(),
+      ],
+    );
+  }
+
+  Widget _buildLineBottom() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(Colors.yellow),
+        _buildDot(Colors.yellow),
+        _buildDot(Colors.yellow),
+        _buildDot(Colors.yellow),
+      ],
+    );
+  }
+
+  Widget _buildDot(Color color) {
+    return Container(
+      width: _size,
+      height: _size,
+      decoration: BoxDecoration(
+        color: _bgColor,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(_borderRadius),
+        ),
+        border: Border.all(
+          color: _borderColor,
+          width: _borderWidth,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmpty() {
+    return const SizedBox(
+      width: _size,
+      height: _size,
+    );
+  }
+}
